823cb018ec4501834d02c61a29fd5e552393a307
imomark <khanomarkhalid@gmail.com>
2021-08-24 12:04:28 +0530

    Initial code base

---
+++ Grid-Operation/src/main/java/com/grid/main/GridOperation/GridOperationApplication.java
@@ -0,0 +1,13 @@
+package com.grid.main.GridOperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridOperationApplication.class, args);
+	}
+
+}
---
+++ Grid-Operation/src/test/java/com/grid/main/GridOperation/GridOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grid.main.GridOperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
